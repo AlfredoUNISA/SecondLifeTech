4672bb6a7f95ac73c5d74afc389c2a166cac76a7
AlfredoUNISA <a.spera28@studenti.unisa.it>
2024-02-15 18:24:18 +0100

    Creazione progetto Spring

---
+++ SecondLifeTech/src/main/java/it/unisa/is/secondlifetech/SecondLifeTechApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.is.secondlifetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondLifeTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondLifeTechApplication.class, args);
+	}
+
+}
---
+++ SecondLifeTech/src/test/java/it/unisa/is/secondlifetech/SecondLifeTechApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisa.is.secondlifetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondLifeTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
