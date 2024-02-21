5b8b0e0262575eeb23f82fce20cbead22968e960
AlfredoUNISA <a.spera28@studenti.unisa.it>
2024-02-21 09:41:32 +0100

    Update UserServiceImplTests.java

--- SecondLifeTech/src/test/java/it/unisa/is/secondlifetech/service/impl/UserServiceImplTests.java
+++ SecondLifeTech/src/test/java/it/unisa/is/secondlifetech/service/impl/UserServiceImplTests.java
@@ -1,27 +1,20 @@
 package it.unisa.is.secondlifetech.service.impl;
 
-import it.unisa.is.secondlifetech.entity.Cart;
-import it.unisa.is.secondlifetech.entity.constant.UserRole;
 import it.unisa.is.secondlifetech.entity.User;
+import it.unisa.is.secondlifetech.entity.constant.UserRole;
 import it.unisa.is.secondlifetech.repository.UserRepository;
-import it.unisa.is.secondlifetech.service.CartService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -58,7 +51,7 @@ class UserServiceImplTests {
 	}
 
 	@Test
-	void UserService_FindByRole_ShouldReturnCorrectList() throws ParseException {
+	void UserService_FindByRole_ShouldReturnCorrectList() {
 		// Arrange
 		String role = user.getRole();
 
