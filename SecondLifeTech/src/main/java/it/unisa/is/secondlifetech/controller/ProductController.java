adc5901fdb95ad325f7c5ba85ec18ccdcd61102a
Giuseppe Giardinetto <g.giardinetto@studenti.unisa.it>
2024-03-05 19:56:22 +0100

    Aggiunta funzionalità per l'aggiunta dalla pagina dettagli

--- SecondLifeTech/src/main/java/it/unisa/is/secondlifetech/controller/ProductController.java
+++ SecondLifeTech/src/main/java/it/unisa/is/secondlifetech/controller/ProductController.java
@@ -34,191 +34,205 @@ import java.util.UUID;
 
 @Controller
 public class ProductController {
-	ProductService productService;
-	UserService userService;
-	ImageFileRepository imageFileRepository;
+    ProductService productService;
+    UserService userService;
+    ImageFileRepository imageFileRepository;
 
-	@Autowired
-	public ProductController(ProductService productService, UserService userService, ImageFileRepository imageFileRepository) {
-		this.productService = productService;
-		this.userService = userService;
-		this.imageFileRepository = imageFileRepository;
-	}
+    @Autowired
+    public ProductController(ProductService productService, UserService userService, ImageFileRepository imageFileRepository) {
+        this.productService = productService;
+        this.userService = userService;
+        this.imageFileRepository = imageFileRepository;
+    }
 
-	@GetMapping("/dashboard-prodotti")
-	public String dashboard(Model model,
-	                        @RequestParam(value = "name", required = false) String name,
-	                        @RequestParam(value = "brand", required = false) String brand,
-	                        @RequestParam(value = "category", required = false) String category,
-	                        @RequestParam(value = "minYear", required = false) Integer minYear,
-	                        @RequestParam(value = "maxYear", required = false) Integer maxYear,
-	                        @RequestParam(value = "minRam", required = false) Integer minRam,
-	                        @RequestParam(value = "maxRam", required = false) Integer maxRam,
-	                        @RequestParam(value = "minDisplaySize", required = false) Double minDisplaySize,
-	                        @RequestParam(value = "maxDisplaySize", required = false) Double maxDisplaySize,
-	                        @RequestParam(value = "minStorageSize", required = false) Integer minStorageSize,
-	                        @RequestParam(value = "maxStorageSize", required = false) Integer maxStorageSize,
-	                        @RequestParam(value = "minPrice", required = false) Double minPrice,
-	                        @RequestParam(value = "maxPrice", required = false) Double maxPrice,
-	                        @RequestParam(value = "color", required = false) String color,
-	                        @RequestParam(value = "state", required = false) String state,
-	                        @RequestParam("page") Optional<Integer> page,
-	                        @RequestParam("size") Optional<Integer> size,
-	                        HttpServletRequest request
-	) throws ErrorInField {
-		int currentPage = page.orElse(1);
-		int pageSize = size.orElse(5);
-		Principal principal = request.getUserPrincipal();
+    @GetMapping("/dashboard-prodotti")
+    public String dashboard(Model model,
+                            @RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "brand", required = false) String brand,
+                            @RequestParam(value = "category", required = false) String category,
+                            @RequestParam(value = "minYear", required = false) Integer minYear,
+                            @RequestParam(value = "maxYear", required = false) Integer maxYear,
+                            @RequestParam(value = "minRam", required = false) Integer minRam,
+                            @RequestParam(value = "maxRam", required = false) Integer maxRam,
+                            @RequestParam(value = "minDisplaySize", required = false) Double minDisplaySize,
+                            @RequestParam(value = "maxDisplaySize", required = false) Double maxDisplaySize,
+                            @RequestParam(value = "minStorageSize", required = false) Integer minStorageSize,
+                            @RequestParam(value = "maxStorageSize", required = false) Integer maxStorageSize,
+                            @RequestParam(value = "minPrice", required = false) Double minPrice,
+                            @RequestParam(value = "maxPrice", required = false) Double maxPrice,
+                            @RequestParam(value = "color", required = false) String color,
+                            @RequestParam(value = "state", required = false) String state,
+                            @RequestParam("page") Optional<Integer> page,
+                            @RequestParam("size") Optional<Integer> size,
+                            HttpServletRequest request
+    ) throws ErrorInField {
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(5);
+        Principal principal = request.getUserPrincipal();
 
-		ProductFilters filters = new ProductFilters(
-			name, brand, category, minYear, maxYear, minRam, maxRam, minDisplaySize, maxDisplaySize,
-			minStorageSize, maxStorageSize, minPrice, maxPrice, color, state
-		);
+        ProductFilters filters = new ProductFilters(
+                name, brand, category, minYear, maxYear, minRam, maxRam, minDisplaySize, maxDisplaySize,
+                minStorageSize, maxStorageSize, minPrice, maxPrice, color, state
+        );
 
-		Page<ProductModel> productPage;
+        Page<ProductModel> productPage;
 
-		if (filters.isDefault()) {
-			// Nessun filtro
-			productPage = productService.findAllModelsPaginated(
-				PageRequest.of(currentPage - 1, pageSize)
-			);
-			model.addAttribute("productPage", productPage);
-			model.addAttribute("productList", productPage.getContent());
+        if (filters.isDefault()) {
+            // Nessun filtro
+            productPage = productService.findAllModelsPaginated(
+                    PageRequest.of(currentPage - 1, pageSize)
+            );
+            model.addAttribute("productPage", productPage);
+            model.addAttribute("productList", productPage.getContent());
 
-		} else {
-			// Applicare i filtri
-			productPage = productService.findAllModelsPaginatedWithFilters(
-				filters,
-				PageRequest.of(currentPage - 1, pageSize)
-			);
-			model.addAttribute("productPage", productPage);
-			model.addAttribute("productList", productPage.getContent());
-		}
+        } else {
+            // Applicare i filtri
+            productPage = productService.findAllModelsPaginatedWithFilters(
+                    filters,
+                    PageRequest.of(currentPage - 1, pageSize)
+            );
+            model.addAttribute("productPage", productPage);
+            model.addAttribute("productList", productPage.getContent());
+        }
 
-		int totalPages = productPage.getTotalPages();
-		if (totalPages > 0) {
-			List<Integer> pageNumbers = new ArrayList<>();
-			for (int i = 1; i <= totalPages; i++) {
-				pageNumbers.add(i);
-			}
-			model.addAttribute("pageNumbers", pageNumbers);
-		}
+        int totalPages = productPage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = new ArrayList<>();
+            for (int i = 1; i <= totalPages; i++) {
+                pageNumbers.add(i);
+            }
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
 
-		model.addAttribute("filters", filters);
-		model.addAttribute("categories", ProductCategory.ALL_CATEGORIES);
-		model.addAttribute("states", ProductState.ALL_STATES);
+        model.addAttribute("filters", filters);
+        model.addAttribute("categories", ProductCategory.ALL_CATEGORIES);
+        model.addAttribute("states", ProductState.ALL_STATES);
 
-		User user = null;
-		if (principal != null) {
-			user = userService.findUserByEmail(principal.getName());
-		}
-		model.addAttribute("user", user);
-		return "dashboard-gestore-prodotti";
-	}
+        User user = null;
+        if (principal != null) {
+            user = userService.findUserByEmail(principal.getName());
+        }
+        model.addAttribute("user", user);
+        return "dashboard-gestore-prodotti";
+    }
 
-	//Gestione dei filtri
-	@PostMapping("/dashboard-prodotti")
-	public String viewProductModelsWithFilters(@ModelAttribute("filters") ProductFilters filters) {
-		String queryString = filters.toQueryString();
-		return "redirect:/dashboard-products?" + queryString;
-	}
+    //Gestione dei filtri
+    @PostMapping("/dashboard-prodotti")
+    public String viewProductModelsWithFilters(@ModelAttribute("filters") ProductFilters filters) {
+        String queryString = filters.toQueryString();
+        return "redirect:/dashboard-products?" + queryString;
+    }
 
-	//Rimozione di un prodotto
-	@PostMapping("/dashboard-prodotti/delete")
-	public String deleteProduct(@RequestParam("id") UUID id) {
-		ProductModel model = productService.findModelById(id);
-		try {
-			productService.deleteModel(model);
-		} catch (Exception e) {
-			return "redirect:/error";
-		}
-		return "redirect:/dashboard-prodotti";
-	}
+    //Rimozione di un prodotto
+    @PostMapping("/dashboard-prodotti/delete")
+    public String deleteProduct(@RequestParam("id") UUID id) {
+        ProductModel model = productService.findModelById(id);
+        try {
+            productService.deleteModel(model);
+        } catch (Exception e) {
+            return "redirect:/error";
+        }
+        return "redirect:/dashboard-prodotti";
+    }
 
-	@PostMapping("/dashboard-prodotti/add")
-	public String otherPost(@ModelAttribute("product") ProductModel model, @RequestParam("image") MultipartFile file) throws IOException, ErrorInField, MissingRequiredField {
-		if (file.isEmpty()) {
-			throw new MissingRequiredField();
-		}
-		ImageFile fileObj = new ImageFile();
-		fileObj.setName(model.getName());
-		fileObj.setContentType(file.getContentType());
-		fileObj.setData(file.getBytes());
-		ImageFile imageFile = imageFileRepository.save(fileObj);
-		model.setImageFile(imageFile);
-		try {
-			productService.createNewModel(model);
-		} catch (ErrorInField errorInField) {
-			throw new ErrorInField("Errore nei campi");
-		} catch (MissingRequiredField missingRequiredField) {
-			throw new MissingRequiredField();
-		} catch (Exception e) {
-			return "redirect:/error";
-		}
-		return "redirect:/dashboard-prodotti";
-	}
+    @PostMapping("/dashboard-prodotti/add")
+    public String otherPost(@ModelAttribute("product") ProductModel model, @RequestParam("image") MultipartFile file) throws IOException, ErrorInField, MissingRequiredField {
+        if (file.isEmpty()) {
+            throw new MissingRequiredField();
+        }
+        ImageFile fileObj = new ImageFile();
+        fileObj.setName(model.getName());
+        fileObj.setContentType(file.getContentType());
+        fileObj.setData(file.getBytes());
+        ImageFile imageFile = imageFileRepository.save(fileObj);
+        model.setImageFile(imageFile);
+        try {
+            productService.createNewModel(model);
+        } catch (ErrorInField errorInField) {
+            throw new ErrorInField("Errore nei campi");
+        } catch (MissingRequiredField missingRequiredField) {
+            throw new MissingRequiredField();
+        } catch (Exception e) {
+            return "redirect:/error";
+        }
+        return "redirect:/dashboard-prodotti";
+    }
 
-	@GetMapping("/dashboard-prodotti/view-variations")
-	public String viewVariations(Model model, Principal principal) {
-		User user = null;
-		if (principal != null) {
-			user = userService.findUserByEmail(principal.getName());
-		}
-		List<ProductVariation> list = productService.findAllVariations();
-		model.addAttribute("user", user);
-		model.addAttribute("variations", list);
-		return "view-variations";
-	}
+    @GetMapping("/dashboard-prodotti/view-variations")
+    public String viewVariations(Model model, Principal principal) {
+        User user = null;
+        if (principal != null) {
+            user = userService.findUserByEmail(principal.getName());
+        }
+        List<ProductVariation> list = productService.findAllVariations();
+        model.addAttribute("user", user);
+        model.addAttribute("variations", list);
+        return "view-variations";
+    }
 
-	@GetMapping("/dashboard-prodotti/add-variation")
-	public String addVariation(Model model, Principal principal) {
-		User user = null;
-		if (principal != null) {
-			user = userService.findUserByEmail(principal.getName());
-		}
-		List<ProductModel> models = productService.findAllModels();
-		model.addAttribute("models", models);
-		model.addAttribute("newVariation", new ProductVariation());
-		model.addAttribute("user", user);
-		return "add-variation";
-	}
+    @GetMapping("/dashboard-prodotti/add-variation")
+    public String addVariation(Model model, Principal principal,
+                               @RequestParam(value = "modelName", required = false) String modelName) {
+        User user = null;
+        List<ProductModel> models;
+        if (principal != null) {
+            user = userService.findUserByEmail(principal.getName());
+        }
+        //Controllo se non è stata richiesto di aggiungere una variazione ad un modello specifico
+        if (modelName != null && !modelName.isBlank()) {
+            models = new ArrayList<>();
+            models.add(productService.findModelByName(modelName));
+        } else {
+             models= productService.findAllModels();
 
-	@PostMapping("/dashboard-prodotti/add-variation")
-	public String addVariation(@ModelAttribute("newVariation") ProductVariation variation,
-	                           @RequestParam("image") MultipartFile file,
-	                           Principal principal) {
-		ProductModel model = null;
-		User user = null;
-		if (principal != null) {
-			user = userService.findUserByEmail(principal.getName());
-		}
-		//Aggiunta Immagine
-		if (!file.isEmpty()) {
-			ImageFile fileObj = new ImageFile();
-			fileObj.setName(variation.getModel().getName());
-			fileObj.setContentType(file.getContentType());
-			try {
-				fileObj.setData(file.getBytes());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			ImageFile imageFile = imageFileRepository.save(fileObj);
-			variation.getModel().setImageFile(imageFile);
-		}
-		//Aggiunta della variazione
+        }
+        model.addAttribute("models", models);
+        model.addAttribute("newVariation", new ProductVariation());
+        model.addAttribute("user", user);
+        return "add-variation";
+    }
 
-		if (variation.getModel().getBrand().isBlank()) { //Se il modello non è presente nel database
-			model = productService.findModelById(variation.getModel().getId());
-		} else {
-			model = variation.getModel();
-		}
-		try {
-			productService.createNewModel(model);
-			productService.createNewVariation(model, variation);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return "redirect:/error";
-		}
-		return "redirect:/dashboard-prodotti/view-variations";
-	}
+    @PostMapping("/dashboard-prodotti/add-variation")
+    public String addVariation(@ModelAttribute("newVariation") ProductVariation variation,
+                               @RequestParam(value = "image", required = false) MultipartFile file,
+                               Principal principal) {
+        ProductModel model = null;
+        User user = null;
+        if (principal != null) {
+            user = userService.findUserByEmail(principal.getName());
+        }
+        //Aggiunta Immagine
+        if (file!=null && !file.isEmpty()) {
+            ImageFile fileObj = new ImageFile();
+            fileObj.setName(variation.getModel().getName());
+            fileObj.setContentType(file.getContentType());
+            try {
+                fileObj.setData(file.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ImageFile imageFile = imageFileRepository.save(fileObj);
+            variation.getModel().setImageFile(imageFile);
+        }
+        //Aggiunta della variazione
+
+        if (variation.getModel().getBrand().isBlank()) { //Se il modello non è presente nel database
+            model = productService.findModelById(variation.getModel().getId());
+        } else {
+            model = variation.getModel(); //Se il modello non è presente nel database
+            try {
+                productService.createNewModel(model); //Aggiungo il modello al database
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "redirect:/error";
+            }
+        }
+        try {
+            productService.createNewVariation(model, variation);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/error";
+        }
+        return "redirect:/dashboard-prodotti/view-variations";
+    }
 }
