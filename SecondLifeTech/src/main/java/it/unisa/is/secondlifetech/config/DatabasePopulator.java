92a779ad6725c6dae0f5ba1e3382b4ee41390d11
dcaprik <127246117+dcaprik@users.noreply.github.com>
2024-03-05 15:00:01 +0100

    Update DatabasePopulator.java

--- SecondLifeTech/src/main/java/it/unisa/is/secondlifetech/config/DatabasePopulator.java
+++ SecondLifeTech/src/main/java/it/unisa/is/secondlifetech/config/DatabasePopulator.java
@@ -4,6 +4,8 @@ import it.unisa.is.secondlifetech.entity.ProductModel;
 import it.unisa.is.secondlifetech.entity.ProductVariation;
 import it.unisa.is.secondlifetech.entity.constant.ProductCategory;
 import it.unisa.is.secondlifetech.entity.constant.ProductState;
+import it.unisa.is.secondlifetech.repository.ProductModelRepository;
+import it.unisa.is.secondlifetech.repository.ProductVariationRepository;
 import it.unisa.is.secondlifetech.service.ProductService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,92 +14,42 @@ import org.springframework.stereotype.Component;
 @Component
 public class DatabasePopulator {
 
-	private final ProductService service;
+	private final ProductModelRepository modelRepository;
+	private final ProductVariationRepository variationRepository;
+
+	private final ProductService productService;
 
 	@Autowired
-	public DatabasePopulator(ProductService service) {
-		this.service = service;
-	}
+	public DatabasePopulator(ProductModelRepository modelRepository, ProductVariationRepository variationRepository, ProductService productService) {
+        this.modelRepository = modelRepository;
+        this.variationRepository = variationRepository;
+        this.productService = productService;
+    }
 
 	@PostConstruct
 	public void populate() {
 		try {
-//			for (int i = 0; i < 5; i++) {
-//				if (service.findModelByName("model" + i) == null) {
-//					String category;
-//
-//					if (i % 2 == 0) {
-//						category = ProductCategory.SMARTPHONE;
-//					} else {
-//						category = ProductCategory.TABLET;
-//					}
-//
-//
-//
-////					for (int j = 0; j < 5; j++) {
-////						ProductVariation variation = getProductVariation(j, i, model);
-////						service.createNewVariation(model, variation);
-////					}
-//				}
-//			}
-//			ProductModel iPhone7 = new ProductModel("iPhone 708", "Apple", ProductCategory.SMARTPHONE);
-//			service.createNewModel(iPhone7);
-//
-//			ProductVariation variation = new ProductVariation(2008,4,5.1,128,800,10,"nero",ProductState.ACCETTABILE,iPhone7);
-//			variation = new ProductVariation(2008,4,5.1,128,120,10,"nero",ProductState.ACCETTABILE,iPhone7);
-//			variation = new ProductVariation(2020,8,6.1,1200,1200,1,"nero",ProductState.ACCETTABILE,iPhone7);
+
+			ProductModel model = new ProductModel("iPhone 7", "Apple", ProductCategory.SMARTPHONE);
+			if (!modelRepository.existsByName(model.getName())) {
+				productService.createNewModel(model);
+				ProductVariation variation = new ProductVariation(2008,4,5.1,128,800,10,"nero",ProductState.ACCETTABILE,model);
+				variation = new ProductVariation(2008,4,5.1,128,120,10,"nero",ProductState.ACCETTABILE,model);
+				variation = new ProductVariation(2020,8,6.1,1200,1200,1,"nero",ProductState.ACCETTABILE,model);
+			}
+
+			model = new ProductModel("iPhone 8", "Apple", ProductCategory.SMARTPHONE);
+			if (!modelRepository.existsByName(model.getName())) {
+				productService.createNewModel(model);
+				ProductVariation variation = new ProductVariation(2008,4,5.1,128,800,10,"nero",ProductState.ACCETTABILE,model);
+				variation = new ProductVariation(2008,4,5.1,128,120,10,"nero",ProductState.ACCETTABILE,model);
+				variation = new ProductVariation(2020,8,6.1,1200,1200,1,"nero",ProductState.ACCETTABILE,model);
+			}
+
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	private static ProductVariation getProductVariation(int j, int i, ProductModel model) {
-		int ram;
 
-		if (j % 2 == 0) {
-			ram = 4;
-		} else {
-			ram = 8;
-		}
-
-		double display;
-
-		if (j % 2 == 0) {
-			display = 5.5;
-		} else {
-			display = 6.0;
-		}
-
-		int storage;
-
-		if (j % 2 == 0) {
-			storage = 64;
-		} else {
-			storage = 128;
-		}
-
-		double price = 100 + i + j;
-
-		String condition;
-
-		if (j % 2 == 0) {
-			condition = ProductState.BUONO;
-		} else {
-			condition = ProductState.OTTIMO;
-		}
-
-
-
-		return new ProductVariation(
-				2020,
-				ram,
-				display,
-				storage,
-				price,
-				j,
-				"color" + j,
-				condition,
-				model
-		);
-	}
 }
