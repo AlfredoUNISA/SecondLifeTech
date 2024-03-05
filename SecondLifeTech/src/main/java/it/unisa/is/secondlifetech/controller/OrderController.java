b8a52d23ff6c65d3e96458e34c7f9e2cb28e96c2
AlfredoUNISA <a.spera28@studenti.unisa.it>
2024-03-05 18:13:41 +0100

    Ordini in ordine discendente

--- SecondLifeTech/src/main/java/it/unisa/is/secondlifetech/controller/OrderController.java
+++ SecondLifeTech/src/main/java/it/unisa/is/secondlifetech/controller/OrderController.java
@@ -1,5 +1,6 @@
 package it.unisa.is.secondlifetech.controller;
 
+import it.unisa.is.secondlifetech.entity.OrderPlaced;
 import it.unisa.is.secondlifetech.entity.User;
 import it.unisa.is.secondlifetech.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class OrderController {
@@ -25,8 +27,12 @@ public class OrderController {
 
 		if (principal != null) {
 			User user = userService.findUserByEmail(principal.getName());
+			List<OrderPlaced> orders = user.getOrders();
+			// sort by date desc
+			orders.sort((o1, o2) -> o2.getDate().compareTo(o1.getDate()));
+
 			model.addAttribute("user", user);
-			model.addAttribute("orders", user.getOrders());
+			model.addAttribute("orders", orders);
 			return "my-profile-orders";
 		}
 
